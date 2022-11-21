f67d2649f9ec04fb02882da80deaccc734a30833
lerzi <569259131@qq.com>
2022-11-21 22:58:47 +0800

    登录页

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,13 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-
+  height: 100%;
   color: #2c3e50;
+}
+body {
+  height: 100%;
+}
+html {
+  height: 100%;
 }
 </style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,9 +1,39 @@
 <template>
-  <div><el-button type="primary">主要按钮</el-button></div>
+  <div class="sb">
+    <el-card class="box-card" shadow="hover">
+      <div slot="header" class="clearfix">
+        <span>登录</span>
+      </div>
+      <div class="ymb">
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+        <el-input
+          placeholder="请输入密码"
+          v-model="input"
+          show-password
+        ></el-input>
+        <el-button type="primary">登录</el-button>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.sb {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+.box-card {
+  width: 450px;
+}
+.ymb {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
